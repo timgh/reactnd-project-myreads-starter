a567ce04fd6890fdd03afe37edacb847113556d4
Tim Renner <tr@Tims-MacBook-Air.local>
2020-04-22 15:49:42 +0200

    move Book, BookList and BookShelf into separate file

--- src/App.js
+++ src/App.js
@@ -1,69 +1,8 @@
 import React from 'react'
 import { Route, Link } from 'react-router-dom'
+import { BookList, BookShelf } from './Books'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
-
-class BookShelf extends React.Component {
-  render() {
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.title}</h2>
-        <div className="bookshelf-books">
-          <BookList books={this.props.books} updateBook={this.props.updateBook} />
-        </div>
-      </div>
-    )
-  }
-}
-
-class BookList extends React.Component {
-  render() {
-    return (
-      <ol className="books-grid">
-        {this.props.books.map((book) => (
-          <li key={book.id}>
-            <Book book={book} updateBook={this.props.updateBook} />
-          </li>
-        ))}
-      </ol>
-    )
-  }
-}
-
-class Book extends React.Component {
-  moveTo = (event) => {
-    const newShelf = event.target.value
-    const book = this.props.book
-    const updateBook = this.props.updateBook
-    console.log(`move ${book.id} from  ${book.shelf} to ${newShelf}`)
-    updateBook({ ...book, shelf:newShelf} , newShelf)
-  }
-
-  render() {
-    const book = this.props.book
-    const image = book.imageLinks ? book.imageLinks.thumbnail : ""
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 192, 
-               backgroundImage: `url(${image})` }}></div>
-          <div className="book-shelf-changer">
-            <select value={book.shelf} onChange={this.moveTo}>
-              <option value="move" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
-        </div>
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors}</div>
-      </div>
-    )
-  }
-}
-
 
 class MainPage extends React.Component {
   render() {
@@ -91,7 +30,6 @@ class MainPage extends React.Component {
     )
   }
 }
-
 
 class SearchPage extends React.Component {
   state = {
